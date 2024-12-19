bdf6404542cd5dfcbc4612c5b46c996e7a14f217
user-0021 <nabeyamaoden@gmail.com>
2024-12-19 15:29:22 +0900

    change example

--- src/main.c
+++ src/main.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 int main(){
-	int first  = nodeSystemAddPipe("First",NODE_CONST,NODE_INT_32,1);
+	int first  = nodeSystemAddPipe("First",NODE_CONST,NODE_INT_8,3);
 	int second = nodeSystemAddPipe("Second",NODE_OUT,NODE_DOUBLE,2);
 	int third  = nodeSystemAddPipe("A",NODE_OUT,NODE_INT_8,1);
+	int forth  = nodeSystemAddPipe("C",NODE_IN,NODE_INT_8,1);
 
 	nodeSystemInit();
 
@@ -13,18 +14,18 @@ int main(){
 
 	nodeSystemDebugLog("SystemBegin");
 
-	int rdata = 0;
+	int8_t rdata[3] = {};
 	char msg[2048];
 
 	while(!nodeSystemLoop()){
 		uint8_t wdata = 0;
-		int ret = nodeSystemRead(first,&rdata,0);
+		int ret = nodeSystemRead(first,rdata,0);
 		if(ret == 1){
-			wdata = rdata + 1;
+			wdata = rdata[0] + 1;
 		}
 
 		nodeSystemWrite(third,&wdata,0);
-		sprintf(msg,"Count %d",rdata);
+		sprintf(msg,"Count %d",(int)rdata[0]);
 		nodeSystemDebugLog(msg);
 		sleep(3);
 	}
