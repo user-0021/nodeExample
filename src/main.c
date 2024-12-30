f4e96a2286c46f5026fe7d75168b92e773495afc
user-0021 <nabeyamaoden@gmail.com>
2024-12-30 12:36:02 +0900

    miner change

--- src/main.c
+++ src/main.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 
 int main(){
-	int first  = nodeSystemAddPipe("First",NODE_CONST,NODE_INT_8,3);
-	int second = nodeSystemAddPipe("Second",NODE_OUT,NODE_DOUBLE,2);
-	int third  = nodeSystemAddPipe("A",NODE_OUT,NODE_INT_8,1);
-	int forth  = nodeSystemAddPipe("C",NODE_IN,NODE_INT_8,1);
+	int8_t constData[3] = {23,12,0};
+
+	int first  = nodeSystemAddPipe("First",NODE_CONST,NODE_INT_8,3,constData);
+	int second = nodeSystemAddPipe("Second",NODE_OUT,NODE_DOUBLE,2,NULL);
+	int third  = nodeSystemAddPipe("A",NODE_OUT,NODE_INT_8,1,NULL);
+	int forth  = nodeSystemAddPipe("C",NODE_IN,NODE_INT_8,1,NULL);
 
 	nodeSystemInit();
 
