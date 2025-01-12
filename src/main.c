02412cd46a4853c4a22b6c4c0c61694c3adebcd4
user-0021 <nabeyamaoden@gmail.com>
2025-01-12 17:27:47 +0900

    update library

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 int main(){
 	int8_t constData[3] = {23,12,0};
 
-	int first  = nodeSystemAddPipe("First",NODE_PIPE_CONST,NODE_UNIT_INT8,3,constData);
+	int first  = nodeSystemAddPipe("First",NODE_PIPE_CONST,NODE_UNIT_INT8,3,NULL);
 	int second = nodeSystemAddPipe("Second",NODE_PIPE_OUT,NODE_UNIT_DOUBLE,2,NULL);
 	int third  = nodeSystemAddPipe("A",NODE_PIPE_OUT,NODE_UNIT_INT8,1,NULL);
 	int forth  = nodeSystemAddPipe("C",NODE_PIPE_IN,NODE_UNIT_INT8,1,NULL);
@@ -20,15 +20,15 @@ int main(){
 	char msg[2048];
 
 	while(!nodeSystemLoop()){
-		// uint8_t wdata = 0;
-		// int ret = nodeSystemRead(first,rdata);
-		// if(ret == 1){
-		// 	wdata = rdata[0] + 1;
-		// }
+		uint8_t wdata = 0;
+		int ret = nodeSystemRead(first,rdata);
+		if(ret == 1){
+			wdata = rdata[0] + 1;
+		}
 
-		// nodeSystemWrite(third,&wdata);
-		// sprintf(msg,"Count %d",(int)rdata[0]);
-		// nodeSystemDebugLog(msg);
-		sleep(3);
+		nodeSystemWrite(third,&wdata);
+		sprintf(msg,"Count %d",(int)rdata[0]);
+		nodeSystemDebugLog(msg);
+		nodeSystemWait();
 	}
 }
